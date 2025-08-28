a37c087a5ef6aea28743de7e9afee1ea680d0b92
M Banshi krishna <mbanshikrishna@gmail.com>
2025-08-28 11:18:06 +0530

    added providers

--- Azure.tf
+++ Azure.tf
@@ -1,1 +1,6 @@
-I am doing some changes in local repositary 
+#I am doing some changes in local repositary 
+# creating a resource group in Azure
+resource "azurerm_resource_group" "Azure Devops" {
+  name     = "Azure devops"
+  location = "south india"
+}
